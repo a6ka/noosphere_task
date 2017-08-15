a446b83495d85982d56cb8495de925bec537af9b
a6ka <a6ka666@gmail.com>
2017-08-15 03:45:03 +0300

    квайн

---
+++ additional_task/quine.php
@@ -0,0 +1,11 @@
+echo 'Самый читерский способ написания квайна - просто убрать тег "&#60;php" :-)';
+
+<?php
+$code = '<?php
+$code = \'\';
+$rcode = str_replace(chr(39), chr(92) . chr(39), $code);
+$code  = str_replace(\'$code = \' . chr(39) . chr(39) . \';\', \'$code = \' . chr(39) . $rcode . chr(39) . \';\', $code);
+print $code . PHP_EOL;';
+$rcode = str_replace(chr(39), chr(92) . chr(39), $code);
+$code  = str_replace('$code = ' . chr(39) . chr(39) . ';', '$code = ' . chr(39) . $rcode . chr(39) . ';', $code);
+print $code . PHP_EOL;
